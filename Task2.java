50dad396db9e947e956d310ef398172e9ab9a949
Nightagent22 <romelbrenio@gmail.com>
2025-11-28 07:26:40 +0000

    Task2

---
+++ Task2.java
@@ -0,0 +1,26 @@
+public class Task2 {
+    public static void main(String[] args) {
+        
+    
+char h = 'H';
+
+byte e= 3;
+
+int l= 1;
+
+int o= 0;
+
+char s = ' ' ;
+
+String n= "world";
+
+float p= 2.0f;
+
+boolean t = true;
+
+String output =  " " + h + e + l +l+ o + s + n + s + p + s + t;
+
+System.out.print(output);
+}
+}
+
